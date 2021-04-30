d382f146848ec9affa357a160981f6bb3cfee141
Zozimo Fernandez <zozi.fer96@gmail.com>
2021-04-30 17:41:44 -0500

    Creando aplicación y mdboostrap

---
+++ fc/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class FootballClub(models.Model):
+    name = models.CharField(verbose_name='Nombre del club', max_length=50)
+    foundation_year = models.CharField(
+        verbose_name='Año de fundación', max_length=50)
+    owner = models.CharField(verbose_name='Dueño', max_length=50)
+    city = models.CharField(verbose_name='Ciudad', max_length=50)
+
+    class Meta:
+        app_label = 'fc'
+        db_table = 'futbolclub'
+        verbose_name = 'Club de futbol'
+        verbose_name_plural = 'Clubs de futbol'
+
+    def __str__(self):
+        return self.name
