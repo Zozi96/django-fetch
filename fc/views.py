1b436f1564486fb0cc84d332ceffc618ebcfd12c
Zozimo Fernandez <zozi.fer96@gmail.com>
2021-04-30 20:25:16 -0500

    Fetch exitoso :D

--- fc/views.py
+++ fc/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import get_list_or_404
 from django.views.generic import CreateView
+from django.http import JsonResponse
 
 from .models import FootballClub
 from .forms import FootballClubForm
@@ -12,7 +12,23 @@ class FootballClubCreateView(CreateView):
     template_name = 'fc/form.html'
 
     def get_context_data(self, **kwargs):
-        context = super(FootballClubCreateView,
-                        self).get_context_data(**kwargs)
+        context = super(
+            FootballClubCreateView,
+            self
+        ).get_context_data(**kwargs)
         context['objects'] = self.model.objects.all()
         return context
+
+    def render_to_response(self, context, **response_kwargs):
+        if self.request.is_ajax():
+            data = list(context['objects'].values())
+            return JsonResponse({'objects': data})
+        else:
+            response_kwargs.setdefault('content_type', self.content_type)
+            return self.response_class(
+                request=self.request,
+                template=self.get_template_names(),
+                context=context,
+                using=self.template_engine,
+                **response_kwargs
+            )
